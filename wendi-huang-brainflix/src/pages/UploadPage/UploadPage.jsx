592a30531e8870017519cf69f062c9b127453942
“Wendi <wendi.huangg@gmail.comgit config user.namegit config user.emailgit config --global init.defaultBranch main git config --global pull.rebase falsegit config --global user.email wendi.huangg@gmail.com>
2024-11-05 14:44:19 -0800

    Upload Page Tablet and Desktop

--- wendi-huang-brainflix/src/pages/UploadPage/UploadPage.jsx
+++ wendi-huang-brainflix/src/pages/UploadPage/UploadPage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import publishIcon from '../../assets/Icons/publish.svg';
+import './UploadPage.scss';
+import UploadPreview from '../../assets/Images/Upload-video-preview.jpg';
+
 
 function UploadPage(){
     const navigate = useNavigate();
@@ -11,19 +15,39 @@ function UploadPage(){
     };
 
     return (
-        <div className="upload-page">
+        <div className="full-page">
             <Header />
-            <h1>Upload Video</h1>
-            <form onSubmit={handleSubmit}>
-                <label>VIDEO THUMBNAIL</label>
-                <img src="" alt="video thumbnail"/>
-                <label>TITLE YOUR VIDEO</label>
-                <input type="text" name="title" required />
-                <label>ADD A VIDEO DESCRIPTION</label>
-                <textarea name="description" required></textarea>
-                <button type="submit">PUBLISH</button>
-                <button type="cancel">CANCEL</button>
-            </form>
+            <hr></hr>
+            <div className="upload-page">
+                <h1 className="upload-page__title">Upload Video</h1>
+                <hr className="upload-page__title-line"></hr>
+                <form onSubmit={handleSubmit}>
+                    <div className="upload-page__form-container">
+                        <div className="upload-page__preview-container">
+                            <label className="upload-page__labels">VIDEO THUMBNAIL</label>
+                            <img src={UploadPreview} alt="video thumbnail" className="upload-page__preview-image"/>
+                        </div>
+                        <div className="upload-page__input-container">
+                            <label className="upload-page__labels">TITLE YOUR VIDEO</label>
+                            <input type="text" name="title" placeholder="Add a title to your video" required />
+                            <label className="upload-page__labels">ADD A VIDEO DESCRIPTION</label>
+                            <textarea name="description" placeholder="Add a description to your video" required></textarea>
+                        </div>
+                    </div>
+                    <hr className="upload-page__title-line"></hr>
+                    <div className="upload-page__button-container">
+                        <button className="upload-page__publish">
+                            <img src={publishIcon} alt='Upload Icon' className='upload-page__publish-icon'/>
+                            <span className='upload-page__publish-text'>PUBLISH</span>
+                        </button>
+                        <button className="upload-page__cancel">CANCEL</button>
+                        <button className="upload-page__publish-big">
+                            <img src={publishIcon} alt='Upload Icon' className='upload-page__publish-icon'/>
+                            <span className='upload-page__publish-text'>PUBLISH</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
